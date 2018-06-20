af747c84ca89fc899ecefafdfc8b51d1bd1dc697
~Lordie <levimanga@gmail.com>
2018-06-20 11:21:20 -0300

    Revert "init ~~ auto roles"
    This reverts commit 1e20f4fd2909d4fa7a192a875fde5e7f3861c5f1.

--- upgrade.js
+++
@@ -1,8 +0,0 @@
-var upgrade = {
-  run: function(Creep){
-    if (Creep.upgradeController(Creep.room.controller) == ERR_NOT_IN_RANGE){
-      Creep.moveTo(Creep.room.controller);
-    }
-  }
-};
-module.exports = upgrade;
